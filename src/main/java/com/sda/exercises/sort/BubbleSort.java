1b9859bb16e2bd5fe1467196246b410fac4fefe1
Kamil Kamiński <kamil991@wp.pl>
2019-06-29 10:15:57 +0200

    Zmiany

---
+++ src/main/java/com/sda/exercises/sort/BubbleSort.java
@@ -0,0 +1,20 @@
+package com.sda.exercises.sort;
+
+import java.util.List;
+
+public class BubbleSort {
+
+    public List<Integer> sort(List<Integer> unsortedList){
+        for (int i=1; i<unsortedList.size(); i++){
+            if(unsortedList.get(i-1)>unsortedList.get(i)){
+                Integer temp = unsortedList.get(i-1);
+                unsortedList.set(i-1, unsortedList.get(i));
+                unsortedList.set(i. temp);
+
+            }
+        }
+        return unsortedList;
+
+    }
+
+}
